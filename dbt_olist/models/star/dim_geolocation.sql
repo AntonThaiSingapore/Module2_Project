7389932e68057701c75c66b339e56a855191bb1c
Anton Thai <anton.thai.sgp@gmail.com>
2026-09-10 23:05:10 +0800

    dim_geolocation.sql

---
+++ dbt_olist/models/star/dim_geolocation.sql
@@ -0,0 +1,25 @@
+-- Create our clean Geolocation table
+
+  {{ config(
+    materialized='table',
+    description="Dimensional table for the geolocation data",
+    enabled=true -- Enable this model       
+  ) }}
+
+SELECT 
+    -- 1. Unique 5-digit Zip Code Prefix (Primary Key)
+    geolocation_zip_code_prefix AS zip_code_prefix,
+    
+    -- 2. Take the average latitude and longitude for this zip code
+    AVG(geolocation_lat) AS latitude,
+    AVG(geolocation_lng) AS longitude,
+    
+    -- 3. ANY_VALUE picks one clean city and state name for this zip code
+    ANY_VALUE(geolocation_city) AS city,
+    ANY_VALUE(geolocation_state) AS state
+
+  -- Start from the raw 1-million-row geolocation table
+  FROM {{ source('olist_eCommerce_1', 'public_olist_geolocation_dataset') }}
+  
+  -- Group all duplicate zip codes together into a single row
+  GROUP BY geolocation_zip_code_prefix
